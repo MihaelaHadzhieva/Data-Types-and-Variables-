1b40e32bdcde2a71e5ced7ebdedafc18a7e5058a
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-20 22:48:01 +0200

    Add files via upload

---
+++ Elavator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = int.Parse(Console.ReadLine());
+            int capacityPeople = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)numberPeople / capacityPeople);
+            Console.WriteLine(courses);
+        }
+    }
+}
