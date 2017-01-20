c7d8f2a02a4f4286695f0dae7b64493abf208ea2
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-20 23:28:37 +0200

    Update CircleArea.cs

--- CircleArea/CircleArea.cs
+++ CircleArea/CircleArea.cs
@@ -10,6 +10,9 @@ namespace CircleArea
     {
         static void Main(string[] args)
         {
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}", Math.PI * radius * radius);
+        
         }
     }
 }
