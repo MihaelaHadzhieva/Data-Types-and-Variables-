6a6df3ec4d404d5dc37a040ee2f0482e07d0b05e
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-20 22:57:51 +0200

    Add files via upload

---
+++ TripplesOfLetters/tripplesOfLetters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLatinLetters
+{
+    class TriplesOfLatinLetters
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int firstLetter = 0; firstLetter < number; firstLetter++)
+            {
+                for (int secondletter = 0; secondletter < number; secondletter++)
+                {
+                    for (int thirdletter = 0; thirdletter < number; thirdletter++)
+                    {
+                        char letter1 = (char)('a' + firstLetter);
+                        char letter2 = (char)('a' + secondletter);
+                        char letter3 = (char)('a' + thirdletter);
+
+                        Console.WriteLine($"{letter1}{letter2}{letter3}");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
