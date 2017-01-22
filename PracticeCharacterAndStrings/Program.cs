59ad5db9008a7a3e9039c739f5ae7c85ef804e1e
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:28:45 +0200

    Add files via upload

---
+++ PracticeCharacterAndStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCharactersАndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstText = "Software University";
+            string secondText = "B";
+            string thirdText = "y";
+            string forthText = "e";
+            string fifthText = "I love programming";
+            Console.WriteLine(firstText);
+            Console.WriteLine(secondText);
+            Console.WriteLine(thirdText);
+            Console.WriteLine(forthText);
+            Console.WriteLine(fifthText);
+        }
+    }
+}
