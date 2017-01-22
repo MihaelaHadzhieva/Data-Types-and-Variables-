b80aaa976140e86e45bb985ecaf5a41efb3a2460
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:23:40 +0200

    Add files via upload

---
+++ ComparingFloats/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            double difference = Math.Abs(firstNum - secondNum);
+            if (difference > 0.000001)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
