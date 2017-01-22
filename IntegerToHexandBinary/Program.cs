43cb6cfc590345b2da9f658fc9b0107d8433d013
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:27:46 +0200

    Add files via upload

---
+++ IntegerToHexandBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToHexandBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int hexadecimalbase = 16;
+            int binarybase = 2;
+            var hexadecimal = Convert.ToString(number,hexadecimalbase);
+            var binary = Convert.ToString(number, binarybase);
+            hexadecimal = hexadecimal.ToUpper();
+            Console.WriteLine(hexadecimal);
+            Console.WriteLine(binary);
+        }
+    }
+}
