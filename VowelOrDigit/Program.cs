d2f5b2197dcd49eb808ba4d578d771f8a8fb8a1f
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:36:19 +0200

    Add files via upload

---
+++ VowelOrDigit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = Console.ReadLine();
+            if (symbol=="0" || symbol == "1" ||
+                symbol == "2" || symbol == "3"
+                || symbol == "4" || symbol == "5"
+                || symbol == "6" || symbol == "7"
+                || symbol == "8" || symbol == "9")
+            {
+                Console.WriteLine("digit");
+            }
+            else if ( symbol == "a" ||
+                symbol == "e" || symbol == "y" || 
+                symbol == "u" || symbol == "o" || symbol=="i")
+            {
+                Console.WriteLine( "vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
