9ba8c0f361bff79666ff2e137db1317390ac6808
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:35:29 +0200

    Add files via upload

---
+++ VariableInHexaemicalFormat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableInHexadecimalFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Text = Console.ReadLine();
+            int number = Convert.ToInt32(Text, 16);
+            Console.WriteLine(number);
+
+        }
+    }
+}
