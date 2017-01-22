51b89815cb0279d595fb74796cd23f4068afc68d
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:21:09 +0200

    Add files via upload

---
+++ BooleanVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fisrtText = Console.ReadLine().ToLower();
+            bool ViewText = Convert.ToBoolean(fisrtText);
+            if (ViewText)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
