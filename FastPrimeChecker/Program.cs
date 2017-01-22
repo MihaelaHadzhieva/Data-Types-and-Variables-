11b8d76a360a20c37abc0219d1a83679ba1fe15e
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-22 14:26:42 +0200

    Add files via upload

---
+++ FastPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 
+            int number = int.Parse(Console.ReadLine());
+            for (int devisor = 2; devisor <= number; devisor++)
+            {
+                bool result = true;
+                for (int dividend = 2; dividend <= Math.Sqrt(devisor); dividend++)
+                {
+                    if (devisor % dividend == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{devisor} -> {result}");
+            }
+                
+         }
+      }
+  }
